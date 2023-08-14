f07fd33c6563c1e70e709683a919cce5818ccab3
rlinane <rlinane@gmail.com>
2023-08-13 21:17:59 -0400

    adding new pages

--- copilot-blazer/obj/Debug/net7.0/copilot-blazer.AssemblyInfo.cs
+++ copilot-blazer/obj/Debug/net7.0/copilot-blazer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("copilot-blazer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46924f14d216b71c61ba38b82ca411159f1305b")]
 [assembly: System.Reflection.AssemblyProductAttribute("copilot-blazer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("copilot-blazer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
